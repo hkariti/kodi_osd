1adfe2c3731eb2a757c308db21035eb0f997c543
Hagai Kariti <hkariti@gmail.com>
2016-10-13 12:10:55 +0300

    Update to support chromecast

--- osd.py
+++ osd.py
@@ -1,10 +1,15 @@
 from __future__ import unicode_literals
 import requests
 import subprocess
+import pychromecast
+import sys
+import time
 
 KODI_HOST = 'osmc.'
 KODI_PORT = 80
 TERMINAL_NOTIFIER = '/Library/Ruby/Gems/2.0.0//gems/terminal-notifier-1.6.3/bin/terminal-notifier'
+GET_WIFI_NAME = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk  '/ SSID/ {print $NF}'"
+MAX_RETRIES = 10
 
 class Kodi(object):
     def __init__(self, host="localhost", port=80):
@@ -12,16 +17,40 @@ class Kodi(object):
     def jsonrpc(self, method, params=dict(), id="id"):
         return requests.post(self._url, json=dict(id=id, jsonrpc="2.0", method=method, params=params)).json()["result"]
 
-kodi = Kodi(KODI_HOST, KODI_PORT)
-players = kodi.jsonrpc('Player.GetActivePlayers')
-if players:
-    playing = kodi.jsonrpc('Player.GetItem', params=dict(playerid=players[0]["playerid"], properties=["track", "artist", "album", "title", "thumbnail"]))
-    artist = ", ".join(playing['item']['artist'])
-    title = playing['item']['title']
-    album = playing['item']['album']
-    track = playing['item']['track']
-    thumbnail = requests.utils.unquote(playing['item']['thumbnail'][8:-1])
+def notification(title, artist, album, thumbnail):
     args = [TERMINAL_NOTIFIER, '-title', 'Now Playing', '-message', "{}\nby {} on {}".format(title, artist, album), '-contentImage', thumbnail, '-sender', 'com.apple.iTunes']
-else:
+    subprocess.Popen(args)
+
+def empty_notif():
     args = [TERMINAL_NOTIFIER, '-title', 'Now Playing', '-message', "Nothing Playing", '-sender', 'com.apple.iTunes']
-subprocess.Popen(args)
+    subprocess.Popen(args)
+
+wifi_name = subprocess.check_output(GET_WIFI_NAME, shell=True).replace("\n", "")
+
+if wifi_name == 'hkariti':
+    kodi = Kodi(KODI_HOST, KODI_PORT)
+    players = kodi.jsonrpc('Player.GetActivePlayers')
+    if players:
+        playing = kodi.jsonrpc('Player.GetItem', params=dict(playerid=players[0]["playerid"], properties=["track", "artist", "album", "title", "thumbnail"]))
+        artist = ", ".join(playing['item']['artist'])
+        title = playing['item']['title']
+        album = playing['item']['album']
+        track = playing['item']['track']
+        thumbnail = requests.utils.unquote(playing['item']['thumbnail'][8:-1])
+        notification(title, artist, album, thumbnail)
+    else:
+        empty_notif()
+elif wifi_name  == 'BigPanda':
+    cast = pychromecast.get_chromecast(friendly_name='Office Music')
+    status = cast.media_controller.status
+    count = 0
+    while status.player_state == 'UNKNOWN':
+        if count == MAX_RETRIES:
+            sys.exit(0)
+        time.sleep(0.1)
+        count += 1
+    if status.images:
+        thumbnail = status.images[0].url
+    else:
+        thumbnail = ''
+    notification(status.title, status.artist, status.album_name, thumbnail)
